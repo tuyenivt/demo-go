59d34efcb2a9dc19b43801c2f4cadb193dd21942
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-17 20:45:37 +0700

    [test] add array and slice implement

--- test/04_array_slice.go
+++ test/04_array_slice.go
@@ -1,9 +1,18 @@
 package main
 
-func Sum(numbers []int) any {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
