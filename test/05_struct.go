1d7125c21f929f550f9f69f605d7d271b942f1e4
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-19 18:15:08 +0700

    [test] add struct implement

---
+++ test/05_struct.go
@@ -0,0 +1,14 @@
+package main
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Width + r.Height)
+}
+
+func Area(r Rectangle) float64 {
+	return r.Width * r.Height
+}
