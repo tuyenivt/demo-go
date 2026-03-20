777bc9fbb7069c76ff8123b6717026c63ac83ad0
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-20 10:43:57 +0700

    [test] add pointer add more test

--- test/06_pointer_test.go
+++ test/06_pointer_test.go
@@ -3,13 +3,27 @@ package main
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	actual := wallet.Balance()
-	expected := Bitcoin(10)
+		actual := wallet.Balance()
+		expected := Bitcoin(10)
 
-	if actual != expected {
-		t.Errorf("expected %s but got %s", expected, actual)
-	}
+		if actual != expected {
+			t.Errorf("expected %s but got %s", expected, actual)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		actual := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if actual != expected {
+			t.Errorf("expected %s but got %s", expected, actual)
+		}
+	})
 }
