8fa01ed2500846285d4e08445bf714c26a843ad5
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-20 10:39:20 +0700

    [test] add pointer refactoring to new type

--- test/06_pointer_test.go
+++ test/06_pointer_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	actual := wallet.Balance()
-	expected := 10
+	expected := Bitcoin(10)
 
 	if actual != expected {
 		t.Errorf("expected %d but got %d", expected, actual)
