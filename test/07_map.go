25fc01df848bbd0701882312fcff69059eb83a66
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-21 18:21:18 +0700

    [test] add map implement with method receiver

--- test/07_map.go
+++ test/07_map.go
@@ -2,6 +2,6 @@ package main
 
 type Dictionary map[string]string
 
-func Search(dictionary Dictionary, word string) string {
-	return dictionary[word]
+func (d Dictionary) Search(word string) string {
+	return d[word]
 }
--- test/07_map_test.go
+++ test/07_map_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	actual := Search(dictionary, "test")
+	actual := dictionary.Search("test")
 	expected := "this is just a test"
 
 	if actual != expected {
