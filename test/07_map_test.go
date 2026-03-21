f6678702b26fe567a2a5857a4850b2f5d141aba3
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-21 18:50:58 +0700

    [test] add map test exception case

--- test/07_map_test.go
+++ test/07_map_test.go
@@ -5,10 +5,25 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	actual := dictionary.Search("test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		actual, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	if actual != expected {
-		t.Errorf("expected %q but got %q given %q", expected, actual, "test")
-	}
+		if actual != expected {
+			t.Errorf("expected %q but got %q given %q", expected, actual, "test")
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		if err.Error() != expected {
+			t.Errorf("expected %q but got %q given %q", expected, err.Error(), "unknown")
+		}
+	})
 }
