0632cb5b27f6f4e619a5c7fa033f8b411af4171f
Tuyen Le Chau <tuyenivt@gmail.com>
2026-03-26 15:15:06 +0700

    [test] add reflection implement

--- test/10_reflection.go
+++ test/10_reflection.go
@@ -2,8 +2,27 @@ package main
 
 import "reflect"
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
 }
