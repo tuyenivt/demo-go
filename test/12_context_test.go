fb4b18329b944d405b48405d669dd8c99cdb93fe
Tuyen Le Chau <tuyenivt@gmail.com>
2026-04-02 17:45:46 +0700

    [test] add context test

---
+++ test/12_context_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello, world"
+	svr := Server(&SpyStore{data})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	response := httptest.NewRecorder()
+
+	svr.ServeHTTP(response, request)
+
+	if response.Body.String() != data {
+		t.Errorf(`expected "%s" but got "%s"`, data, response.Body.String())
+	}
+}
